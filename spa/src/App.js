7c4b0ad80c2cd52e2ec9a9ecac1b50a8a1fffe2c
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2019-10-12 19:40:02 -0300

    parcial

---
+++ spa/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Login from './components/login/Login';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Teste 123</h2>
+                <Login />
+            </div>
+
+        )
+
+    }
+
+
+}
+
+export default App;
---
+++ spa/src/components/login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <h2>Minha tela de Login</h2>
+        )
+
+    }
+
+
+}
+
+
+export default Login;
